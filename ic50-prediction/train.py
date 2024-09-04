91df8dea68619feb35116c6970e2065b0d741b7d
GangBean <xbox2537@gmail.com>
2024-09-04 15:30:06 +0900

    feat: implements random seed fix #6

--- ic50-prediction/train.py
+++ ic50-prediction/train.py
@@ -2,9 +2,12 @@ import hydra
 
 from torch.utils.data import DataLoader
 
-from dataset import DataPreprocess, IC50Dataset
+from dataset import (
+    DataPreprocess, IC50Dataset,
+    SimpleDNNPreprocess, SimpleDNNDataset,
+)
 from trainers import Trainer
-from utils import log_wandb
+from utils import log_wandb, set_seed
 
 from omegaconf import DictConfig
 from loguru import logger
@@ -13,16 +16,25 @@ from loguru import logger
 @hydra.main(version_base=None, config_path="./", config_name="train_config")
 @log_wandb
 def run(cfg: DictConfig):
-    logger.info("[Train] start train...")
+    set_seed(cfg.seed)
 
+    logger.info("[Train] start train...")
     logger.info("[Train] 1. preprocess data...")
-    preprocess = DataPreprocess(cfg.data_dir)
+    if cfg.model_name in ('dnn', ):
+        preprocess = SimpleDNNPreprocess(cfg.data_dir)
+    else:
+        preprocess = DataPreprocess(cfg.data_dir)
 
     logger.info("[Train] 2. split data...")
     train_df, valid_df, test_df = preprocess.split()
-    train_data = IC50Dataset(train_df, train=True)
-    valid_data = IC50Dataset(valid_df, train=True)
-    test_data = IC50Dataset(test_df, train=False)
+    if cfg.model_name in ('dnn', ):
+        train_data = SimpleDNNDataset(train_df, train=True)
+        valid_data = SimpleDNNDataset(valid_df, train=True)
+        test_data = SimpleDNNDataset(test_df, train=False)
+    else:
+        train_data = IC50Dataset(train_df, train=True)
+        valid_data = IC50Dataset(valid_df, train=True)
+        test_data = IC50Dataset(test_df, train=False)
 
     logger.info("[Train] 3. prepare dataloader...")
     train_dataloader = DataLoader(train_data, batch_size=cfg.batch_size)
@@ -39,8 +51,7 @@ def run(cfg: DictConfig):
     trainer.load_best_model()
     submission = trainer.evaluate(test_dataloader)
     trainer.inference(submission)
-
-    logger.info("[Train] end train...")
+    logger.info("[Train] end run...")
 
 if __name__ == '__main__':
     run()
